3f2a6d4f984fac206216ac8e5f46e0ddd2883d68
Curtis Schiewek <curtis.schiewek@gmail.com>
2016-06-08 21:35:55 -0400

    changeset tweaks and validations, refs #1

--- test/models/redirect_test.exs
+++ test/models/redirect_test.exs
@@ -4,15 +4,35 @@ defmodule Extemporize.RedirectTest do
   alias Extemporize.Redirect
 
   @valid_attrs %{destination: "some content", pattern: "some content"}
-  @invalid_attrs %{}
+  @invalid_attrs %{destination: " ", pattern: " "}
+
+  test "attributes exist" do
+    for attr <- [:destination, :pattern], do: assert %Redirect{} |> Map.keys |> Enum.member?(attr)
+  end
 
   test "changeset with valid attributes" do
     changeset = Redirect.changeset(%Redirect{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Redirect.changeset(%Redirect{}, @invalid_attrs)
-    refute changeset.valid?
+  test "changeset without required fields" do
+    for attr <- [:destination, :pattern] do
+      changeset = Redirect.changeset(%Redirect{}, Map.delete(@valid_attrs, attr))
+      refute changeset.valid?
+    end
+  end
+
+  test "string fields gets whitespace stripped" do
+    for field <- [:pattern, :destination] do
+      value = Map.get(@valid_attrs, field) <> " "
+      changeset = Redirect.changeset(%Redirect{}, Map.put(@valid_attrs, field, value))
+      assert changeset |> Map.get(:changes) |> Map.get(field) == Map.get(@valid_attrs, field)
+    end
+  end
+
+  test "changeset with duplicate pattern" do
+    Repo.insert! Redirect.changeset(%Redirect{}, @valid_attrs)
+    {result, _} = Repo.insert Redirect.changeset(%Redirect{}, @valid_attrs)
+    assert result == :error
   end
 end
--- web/models/redirect.ex
+++ web/models/redirect.ex
@@ -14,12 +14,25 @@ defmodule Extemporize.Redirect do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:pattern, :destination])
+    |> strip_whitespace([:pattern, :destination])
     |> validate_required([:pattern, :destination])
+    |> unique_constraint(:pattern)
   end
 
   def match(query \\ __MODULE__, pattern) do
-    from r in query,
-    select: [:destination],
-    where: r.pattern == ^pattern
+    from r in query, select: [:destination], where: r.pattern == ^pattern
+  end
+
+  defp strip_whitespace(changeset, fields) when is_list(fields) do
+    Enum.reduce(fields, changeset, &strip_whitespace(&2, &1))
+  end
+
+  defp strip_whitespace(changeset, field) do
+    if Map.has_key?(changeset.changes, field) do
+      value = get_change(changeset, field) |> String.strip
+      put_change(changeset, field, value)
+    else
+      changeset
+    end
   end
 end
